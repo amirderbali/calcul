a17ff82d1d70b2b0ca6784fed5afc6af5160408f
AMIRR <amirderbali81@gmail.com>
2026-04-22 02:27:16 +0200

    calculatrice

---
+++ calculatrice.py
@@ -0,0 +1,16 @@
+import unittest
+from test import addition, soustraction, multiplication
+
+class TestCalculator(unittest.TestCase):
+
+    def test_addition(self):
+        self.assertEqual(addition(2, 3), 5)
+
+    def test_soustraction(self):
+        self.assertEqual(soustraction(5, 3), 2)
+
+    def test_multiplication(self):
+        self.assertEqual(multiplication(2, 4), 8)
+
+if __name__ == '__main__':
+    unittest.main()
