3e6d643ae184f3f7f9f471d94fa3a768eec44857
AMIRR <amirderbali81@gmail.com>
2026-04-28 18:44:51 +0200

    connexion 2

--- send_results.py
+++ send_results.py
@@ -5,11 +5,11 @@ import os
 # ============================================================
 # === CONFIGURATION ODOO ===
 # ============================================================
-ODOO_URL      = "http://192.168.1.3:8069"
+ODOO_URL      = "http://localhost:8069"
 ODOO_DB       = "test_management"
 ODOO_USER      = "admin"
 # On essaie de lire une variable d'env, sinon on utilise "admin" en dur
-ODOO_PASSWORD  = "admin" 
+ODOO_PASSWORD  = "eb0b6acea110d3001d1f09ae07b570aa50fe7a51" 
 
 # ============================================================
 # CONNEXION ODOO
