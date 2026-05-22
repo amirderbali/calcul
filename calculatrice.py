dcfa4eb3774c19072632adce0ab658f219817be9
AMIRR <amirderbali81@gmail.com>
2026-05-22 16:01:58 +0200

    tester credentials

--- calculatrice.py
+++ calculatrice.py
@@ -10,7 +10,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(soustraction(5, 3), 2)
 
     def test_multiplication(self):
-        self.assertEqual(multiplication(2, 4), 8)
+        self.assertEqual(multiplication(2, 4), 1)
 
 if __name__ == '__main__':
     unittest.main()
