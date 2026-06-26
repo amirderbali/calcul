a254ad67b0f391603291d59da5a3f4fb73f13612
AMIRR <amirderbali81@gmail.com>
2026-06-26 18:37:37 +0200

    chnages

--- calculatrice.py
+++ calculatrice.py
@@ -51,15 +51,18 @@ class TestCalculatorUI(unittest.TestCase):
 
     def setUp(self):
         chrome_options = Options()
-        self.is_jenkins = "JENKINS_URL" in os.environ
+        self.is_jenkins = any(v in os.environ for v in ("BUILD_NUMBER", "JENKINS_URL", "CI"))
         if self.is_jenkins:
-            chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--headless=new")
             chrome_options.add_argument("--no-sandbox")
             chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument("--disable-gpu")
+            chrome_options.add_argument("--window-size=1920,1080")
         else:
             chrome_options.add_experimental_option("detach", True)
 
         self.driver = webdriver.Chrome(options=chrome_options)
+        self.driver.set_page_load_timeout(30)
 
         current_dir = os.path.dirname(os.path.abspath(__file__))
         chemin_complet = os.path.join(current_dir, 'web.html')
